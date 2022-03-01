33311fda690c6fd0779c13b99db8f6e3c92e1c95
Omar <ojrojasr@hotmail.com>
2022-03-01 13:57:27 -0300

    Creating the boostrap NavBar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBarComponent from './components/navBarComponent'
+
+function App() {
+  return <NavBarComponent/>
+}
+
+export default App;
