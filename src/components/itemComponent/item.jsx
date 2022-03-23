c1e76695bc6f2fb6e30c9d503f95ac084f2f0d66
Omare123 <ojrojasr@hotmail.com>
2022-03-22 22:04:28 -0300

    Router

--- src/components/itemComponent/item.jsx
+++ src/components/itemComponent/item.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ItemCount from "../itemCount/itemCount";
+import { Link } from 'react-router-dom'
 import "./item.scss"
 
-export default function Item({ name, price, imageUrl, setItemDetail }) {
+export default function Item({ id, name, price, imageUrl, setItemDetail }) {
   function selectItem(){
     setItemDetail({
       name: name,
@@ -10,15 +11,15 @@ export default function Item({ name, price, imageUrl, setItemDetail }) {
       imageUrl: imageUrl
     })
   }
-  return (<div onClick={selectItem} className="item">
-    <div className="item__image">
-      <img className="image" src={imageUrl} />
-    </div>
-    <div className="item__description">
-      <span>{name}</span>
-      <span>{price}</span>
-      <ItemCount inicial={1} stock={6} />
-    </div>
-  </div>)
+  return (<Link to={`/item/${id}`}><div onClick={selectItem} className="item">
+  <div className="item__image">
+    <img className="image" src={imageUrl} />
+  </div>
+  <div className="item__description">
+    <span>{name}</span>
+    <span>{price}</span>
+    <ItemCount inicial={1} stock={6} />
+  </div>
+</div></Link>)
 
 }
