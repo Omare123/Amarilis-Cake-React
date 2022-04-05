097bf2a33cc4ec7a2f7fef3ccf7d31c9f888f3e7
Omar <ojrojasr@hotmail.com>
2022-04-05 19:12:22 -0300

    firebase

--- src/components/itemDetailsContainer/itemDetailsContainer.jsx
+++ src/components/itemDetailsContainer/itemDetailsContainer.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ItemDetail from "../itemDetailComponent/itemDetail";
+import { getDoc, doc } from 'firebase/firestore'
+import { db } from '../../utils/firebase'
 import "./itemDetailsContainer.scss"
+import { useParams } from "react-router-dom";
 
-export default function ItemDetailsContainer({item}) {
-
+export default function ItemDetailsContainer() {
+  let { id } = useParams();
+  const [item, setItem] = useState()
+  useEffect(() => {
+    const getData = async() => {
+      const query  = doc(db, 'items', id)
+      const response = await getDoc(query)
+      const data = response.data()
+      setItem({id: response.id, ...data})
+    }
+    getData()
+  }, [])
   return (<div className="detailsContainer">
     {item && <ItemDetail item={item}/>}
   </div>)
